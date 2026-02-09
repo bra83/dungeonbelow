26923e24936d47f5ffcb3dd9ffdbc6232077b451
bra83 <55473935+bra83@users.noreply.github.com>
2026-02-09 00:29:15 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1084,4 +1084,4 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(<React.StrictMode><App /></React.StrictMode>);
-        }
+                        }
